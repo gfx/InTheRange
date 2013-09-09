ff72b2561338c5d391f70a1d9dee837332bc520a
Fuji, Goro <goro-fuji@cookpad.com>
2013-09-09 09:04:04 +0900

    format with option+cmd+l

--- InTheRange/src/main/java/com/github/gfx/intherange/MainActivity.java
+++ InTheRange/src/main/java/com/github/gfx/intherange/MainActivity.java
@@ -16,15 +16,14 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_main);
+
         final Fragment fragment = getFragmentManager().findFragmentById(R.id.map);
-        final GoogleMap map = ((MapFragment)fragment).getMap();
+        final GoogleMap map = ((MapFragment) fragment).getMap();
         map.setMyLocationEnabled(true);
 
         final LatLng mtFujiCoord = new LatLng(35.3580, 138.7310);
-        map.moveCamera( CameraUpdateFactory.newLatLngZoom(mtFujiCoord, 10.0f) );
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(mtFujiCoord, 10.0f));
 
     }
 
@@ -34,5 +33,5 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
 }
